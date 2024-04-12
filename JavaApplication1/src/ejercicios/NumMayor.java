73ac9f2f14a443eb9efd4ac2ff766df706d1af03
Marcos Gomez <marcoslg.gomez@gmail.com>
2024-04-12 19:28:20 -0300

    fix

--- JavaApplication1/src/ejercicios/NumMayor.java
+++ JavaApplication1/src/ejercicios/NumMayor.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package ejercicios;
 
-/**
- *
- * @author Marcos
- */
 public class NumMayor {
-    
+    public static void main(String[] args) {
+        
+    }
 }
