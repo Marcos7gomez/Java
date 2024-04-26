8c6d04fca5a0cbc90815e2f4491a289452a14e81
Marcos Gomez <marcoslg.gomez@gmail.com>
2024-04-26 16:03:13 -0300

    add crud definitivo

---
+++ CRUDJAVAMYSQL/src/main/java/com/mycompany/crudjavamysql/CRUDJAVAMYSQL.java
@@ -0,0 +1,13 @@
+
+
+package com.mycompany.crudjavamysql;
+
+
+public class CRUDJAVAMYSQL {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        
+    }
+}
